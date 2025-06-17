c1a0162bfa40283a67f2fb6a9578467b679ec576
Ricardo Zimmer Junior <108034218+Ricardozj1@users.noreply.github.com>
2025-06-17 13:30:52 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -2,24 +2,34 @@ CREATE DATABASE gestao_estoque;
 USE gestao_estoque;
 
 -- Tabela Categoria
-CREATE TABLE categoria (
-    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(50) NOT NULL,
-    tamanho ENUM('Pequeno', 'Médio', 'Grande') NOT NULL,
-    embalagem ENUM('Lata', 'Vidro', 'Plástico') NOT NULL
+CREATE TABLE tb_categoria (
+    id_categoria INT PRIMARY KEY AUTO_INCREMENT,
+    nome_categoria VARCHAR(100) NOT NULL,
+    tamanho VARCHAR(100) NOT NULL,
+    embalagem VARCHAR(100) NOT NULL
 );
 
 -- Tabela Produto
-CREATE TABLE produto (
-    id_produto INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    preco_unitario DECIMAL(10,2) NOT NULL,
-    unidade VARCHAR(20) NOT NULL,
-    quantidade_estoque INT NOT NULL DEFAULT 0,
-    quantidade_minima INT NOT NULL,
-    quantidade_maxima INT NOT NULL,
+CREATE TABLE tb_produto (
+	id_produto INT PRIMARY KEY AUTO_INCREMENT,
+    nome_produto VARCHAR(100) NOT NULL,
+    preco DOUBLE NOT NULL,
+    unidade VARCHAR(100) NOT NULL,
+    quantidade_produto INT NOT NULL,
+    quantidadeMax INT NOT NULL,
+    quantidadeMin INT NOT NULL,
     id_categoria INT NOT NULL,
-    FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria)
+    FOREIGN KEY (id_categoria) REFERENCES tb_categoria(id_categoria) ON DELETE CASCADE
+);
+
+-- Tabela Movimentação
+CREATE TABLE tb_movimentacao (
+	id_movimentacao INT PRIMARY KEY AUTO_INCREMENT,
+    data DATE NOT NULL,
+    quantidade_movimentacao INT NOT NULL,
+    tipo varchar(100) NOT NULL,
+    id_produto INT NOT NULL,
+    FOREIGN KEY (id_produto) REFERENCES tb_produto(id_produto) ON DELETE CASCADE
 );
 
 show tables;
